e5d6638d2a68ee8c327cfdf3674e4caf120756f1
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-09-24 11:05:35 +0200

    Adds simple error handling

--- parser.js
+++ parser.js
@@ -2,14 +2,30 @@ const antlr4 = require("antlr4");
 const Lexer = require("./lib/mathLexer").mathLexer;
 const Parser = require("./lib/mathParser").mathParser;
 
+class ErrorHandler {
+  syntaxError(_recognizer, _offendingSymbol, _line, _column, errorMsg) {
+    const error = new Error(errorMsg);
+    error.stack = "";
+    throw error;
+  }
+}
+
 module.exports = input => {
   const chars = new antlr4.InputStream(input);
   const lexer = new Lexer(chars);
 
   const tokens = new antlr4.CommonTokenStream(lexer);
-  const parser = new Parser(tokens);
 
-  const tree = parser.start();
+  try {
+    const parser = new Parser(tokens);
 
-  return tree;
+    parser.removeErrorListeners();
+    parser.addErrorListener(new ErrorHandler());
+
+    const tree = parser.start();
+
+    return tree;
+  } catch (error) {
+    throw error;
+  }
 };
